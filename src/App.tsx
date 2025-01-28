49a06ca3e5af4c0f7fb0d27d2a6b4a625d5cfff4
Joshua Batties <joshbatties.dev@gmail.com>
2025-01-29 02:09:20 +1100

    remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import ShipmentTracker from './components/tracking/ShipmentTracker';
 const App: React.FC = () => {
   return (
     <HashRouter>
-      <Head /> {/* Added Head component */}
+      <Head /> 
       <div className="min-h-screen flex flex-col">
         <Header />
         <main className="flex-1 flex flex-col overflow-auto">
